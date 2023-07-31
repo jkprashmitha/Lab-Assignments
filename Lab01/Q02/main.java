c539d5ee503477f2c9322c30b35aa21710ea4977
jkprashmitha <137058770+jkprashmitha@users.noreply.github.com>
2023-07-31 13:43:01 +0530

    Add files via upload

---
+++ Lab01/Q02/main.java
@@ -0,0 +1,8 @@
+public class Second
+{
+	public static void main(String[] args)
+		{
+			System.out.println("Prabasha Rashmitha");
+			System.out.println("Management Information System");
+		}
+}
---
+++ Lab04/Q02/SuperB.java
@@ -0,0 +1,27 @@
+
+package com.mycompany.prac04q02;
+
+public class SuperB
+{
+    int b;
+    void setIt (int n) 
+    { 
+        b=n; 
+    }
+    
+    void increase () 
+    {
+        b=b+1;
+    }
+    
+    void triple () 
+    {
+        b=b*3;
+    };
+    
+    int returnIt () 
+    {
+        return b;
+    }
+
+}
